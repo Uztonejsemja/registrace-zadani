b7c178cdfc70bc07d21e78223c3e6d5f14ab4b0c
Uztonejsemja <98396890+Uztonejsemja@users.noreply.github.com>
2022-09-25 18:31:00 +0200

    registration form with fixed alerts

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
-import './Registration.css'
+import './Registration.css';
+import icon from './img/icon.png';
 
 const Registration = () => {
 
@@ -8,18 +9,90 @@ const Registration = () => {
     username: '',
     email: '',
     password: '',
-    passwordConfirm: ''
+    passwordConfirm: '',
   });
 
-  return(
+  const [error, setError] = useState({});
+  const [valid, setValid] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setError(validate(user));
+    setValid(true);
+    console.log(user);
+    setUser({
+      username: '',
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    });
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  useEffect(() => {
+    if (user.email.indexOf('@') >= 0 && user.username.length === 0) {
+      setUser({
+        ...user,
+        username: user.email.split('@')[0],
+      });
+    }
+  }, [user.email]);
+
+  const validate = (values) => {
+    const errorMsgs = {};
+    if (!values.email) {
+      alert('Enter your email');
+    }
+    if (!values.password) {
+      alert('Password is required');
+    }
+    if (values.passwordConfirm !== values.password) {
+      alert('Passwords have to match');
+    }
+    else return alert('Successfully submitted')
+  };
+
+  return (
     <>
-    <form>
+      <form onSubmit={handleSubmit}>
+        <img src={icon} alt="icon"></img>
+        <input
+          type="email"
+          placeholder="Email Address"
+          name="email"
+          value={user.email}
+          onChange={handleChange}
+        />
         
-        <input type="email" id="email" placeholder="Email Address" required></input>
-        <input type="text" id="username" placeholder="User Name"></input>
-        <input type="password" id="password" placeholder="Password"></input>
-        <input type="password" id="passwordConfirm" placeholder="Confirm Password"></input>
-        <button>REGISTER</button>
+        <input
+          type="text"
+          placeholder="User Name"
+          name="username"
+          disabled
+          value={user.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={user.password}
+          onChange={handleChange}
+        />
+        
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          name="passwordConfirm"
+          value={user.passwordConfirm}
+          onChange={handleChange}
+        />
+
+        <button type="submit">REGISTER</button>
       </form>
     </>
   );
